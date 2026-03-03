64f6e1fa6eb5188053cae146f951dbdf1014b0c3
Jakob Zahn <jakob.zahn@mailbox.org>
2026-03-03 14:21:16 +0100

    Enable TLS automatically for client connections to non-localhost hosts
    The `--safe` and `--unsafe` CLI options are thereby obsolete.
    Probing for TLS is removed.
    The client's TLS context is explicitly set to only allow connections
    with TLS v1.2 or higher.

--- src/elva/apps/chat/app.py
+++ src/elva/apps/chat/app.py
@@ -349,7 +349,6 @@ class UI(App):
                 c["identifier"],
                 c["host"],
                 port=c.get("port"),
-                safe=c.get("safe"),
                 on_exception=self.on_provider_exception,
             )
 
